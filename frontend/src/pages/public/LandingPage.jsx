291f9828798026d11e97894440e726cfd79afe0b
Isabela <isabela.bermudez@correounivalle.edu.co>
2026-04-09 20:00:19 -0500

    Implementation of global variables and styles, and the button component

--- frontend/src/pages/public/LandingPage.jsx
+++ frontend/src/pages/public/LandingPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import Button from "../../components/ui/Button/Button.jsx";
 
 const LandingPage = () => {
   return (
     <div>
-      LandingPage
+      LandingPage yooy
+      <Button
+        label="Inicia sesión"
+        variant="filled"
+        size="medium"
+        color="var(--color-pink-light)"
+        onClick={() => console.log("funciona!")}
+      />
       {/* TODO: implementar landing pública de DanceLearn Academy */}
     </div>
   );
 };
 
 export default LandingPage;
-
