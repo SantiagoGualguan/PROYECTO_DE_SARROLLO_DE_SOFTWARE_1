d8c96bce34a9838fbb1c48e3e53b300df206d058
Isabela <isabela.bermudez@correounivalle.edu.co>
2026-06-05 17:51:23 -0500

    feat: add Bootstrap 5.3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/variables.css";
 import "./styles/global.css";
 import App from "./App";
 import "./index.css";
-import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
